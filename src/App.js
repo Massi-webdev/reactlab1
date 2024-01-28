aecc4dfe6ccd9e47be9a2de0a803c94939b33c18
Massi-webdev <ouiddir.massia@gmail.com>
2024-01-28 12:57:31 -0500

    added videos and audio elements + used react-video package downloaded form an npmjs.com

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import Contact from "./Contact";
 import CurrentTime from "./CurrentTime";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import logo from "./assets/images/logo.png"
+import ReactPlayer from 'react-player'
 
 function App() {
 
-  const randomImgUrl = "https://picsum.photos/400/265"
+  const randomImgUrl = "https://picsum.photos/400/265";
+
+  const RandomYTVideo = () => { return (<ReactPlayer url="https://www.youtube.com/watch?v=cZB0NO0jnz0" playing={false} volume={0.5} /> );}; 
+
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  };
+
+
 
   return (
     <BrowserRouter>
@@ -18,6 +35,7 @@ function App() {
         <img src={logo} alt="a logo image" width={"35px"} height={"35px"} />
         <img src={require("./assets/images/logo.png")} alt="a logo image" width={"35px"} height={"35px"} />
         <img src={randomImgUrl} alt="a logo image" width={"35px"} height={"35px"} />
+        
 
           <Link to="/" className="nav-item">Homepage</Link>
           <Link to="/about" className="nav-item">About Little Lemon</Link>
@@ -33,9 +51,13 @@ function App() {
         </Routes>
 
 
-
+        <RandomYTVideo />
+        
       </div>
-
+      
+      <div>
+        <button onClick={toggle1}>Caspian Tern 1</button>
+      </div>  
 
     </BrowserRouter>
 
